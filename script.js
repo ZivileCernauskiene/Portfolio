d33e022d6e1b4dca5dc93ba263c419fe08684418
ZivileCernauskiene <labadaba@gmail.com>
2021-09-24 17:42:46 +0300

    užbaigtas klausimynas (be klausimų)
    užbaigtas klausimynas, padarytas pabaigos puslapis, šoninės nuorodos į kitus tinklapius.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 console.log("veikia")
 
+// let button = document.getElementById("superbtn");
+// button.onclick = function(event) {
+//     button.scrollIntoView()
+// }
+
 function Homepage() {
     window.location.href = "index.html"
 }
 
-let a = 0;
+
 
 let atsakymai = [
     "kl1taip", "kl2taip", "kl3taip", "kl4taip", "kl5taip"
@@ -14,8 +19,11 @@ function start() {
     document.getElementsByClassName("kl1")[0].style.display = "flex";
     document.getElementsByClassName("start")[0].style.display = "none";
 }
+let a = 0;
 
 function funk1() {
+
+    let abc = a;
     document.getElementsByClassName("kl1")[a].style.display = "none";
     if (document.getElementById(atsakymai[a]).checked) {
         a++
@@ -23,6 +31,11 @@ function funk1() {
     } else {
         document.getElementsByClassName("error")[0].style.display = "flex"
     }
+}
+
+function end() {
+    document.getElementsByClassName("kl1")[a].style.display = "none";
+    document.getElementsByClassName("pabaiga")[0].style.display = "flex";
 }
 
 function restart() {
